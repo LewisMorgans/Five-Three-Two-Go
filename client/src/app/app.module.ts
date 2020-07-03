9654c5c163387475dece3921d272aed807ddc653
Lewis Morgans <lmorgans90@gmail.com>
2020-07-03 17:17:42 +0100

    update designs

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -2,10 +2,24 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { IndexPageComponent } from './pages/index-page/index-page.component';
+import { RegisterComponentComponent } from './pages/register-page/register-component.component';
+import { LoginPageComponent } from './pages/login-page/login-page.component';
+import { ChartsPageComponent } from './pages/charts-page/charts-page.component';
+import { AccountPageComponent } from './pages/account-page/account-page.component';
+import { FinancesPageComponent } from './pages/finances-page/finances-page.component';
+import { NavbarComponentComponent } from './components/navbar/navbar-component.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    IndexPageComponent,
+    RegisterComponentComponent,
+    LoginPageComponent,
+    ChartsPageComponent,
+    AccountPageComponent,
+    FinancesPageComponent,
+    NavbarComponentComponent
   ],
   imports: [
     BrowserModule
