680248487323cf7925bc19b3a9324e750b0239ec
Lewis Morgans <lmorgans90@gmail.com>
2020-07-06 17:55:15 +0100

    init

--- client/src/app/pages/modal-page/modal-page.component.ts
+++ client/src/app/pages/modal-page/modal-page.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-modal-page',
   templateUrl: './modal-page.component.html',
-  styleUrls: ['./modal-page.component.css']
+  styleUrls: ['./modal-page.component.scss']
 })
 export class ModalPageComponent implements OnInit {
 
