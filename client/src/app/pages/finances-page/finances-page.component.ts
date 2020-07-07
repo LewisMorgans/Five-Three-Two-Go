0157cde80a698f22335880d8c50fc07198d811a3
Lewis Morgans <lmorgans90@gmail.com>
2020-07-07 15:47:34 +0100

    base work on financePage

--- client/src/app/pages/finances-page/finances-page.component.ts
+++ client/src/app/pages/finances-page/finances-page.component.ts
@@ -19,52 +19,27 @@ export class FinancesPageComponent implements OnInit {
   public barChartType: ChartType = 'bar';
   public barChartLegend = true;
   public barChartData: ChartDataSets[]
+  public five;
+  public three;
+  public two;
 
   constructor(private readonly _dataService: DataRetrievalService) {
-    // this.setChartData$();
   }
 
   ngOnInit() {
-    this.setChartData$()
-  }
-
-  public async setValues(e: Event) {
-
-     let values = {
-      five: e[0], // 500
-      three: e[1], // 300
-      two: e[2] // 200
-    }
-
-    let result = await values;
-
-    return result;
 
   }
 
-  private setChartData$() {
+  public setValues(e: Event) {
 
-    this.setValues() // missing event value, not sure what to do for the param?
-      .then((value) => {
-        this.barChartData = [
-          { data: [value.five, value.three, value.two], label: 'Budget', backgroundColor: ['#364F6B', '#364F6B', '#364F6B'] },
-          { data: [1020, 740, 594.60], label: 'Actual Spend', backgroundColor: ['#d72323', '#d72323', '#d72323'] }
-        ];
-      })
+    this.barChartData = [
+      { data: [e[0], e[1], e[2], 0], label: 'Budget', backgroundColor: ['#364F6B', '#364F6B', '#364F6B'] },
+      { data: [1020, 740, 594.60], label: 'Actual Spend', backgroundColor: ['#d72323', '#d72323', '#d72323'] }
+    ];
 
-
-
-
-
-
-
-    // this._dataService.getFinances$()
-    //   .subscribe(resp => {
-    //     this.barChartData = [
-    //       { data: [this.fifty, this.thirty, this.twenty], label: 'Budget', backgroundColor: ['#364F6B', '#364F6B', '#364F6B'] },
-    //       { data: [1020, 740, 594.60], label: 'Actual Spend', backgroundColor: ['#d72323', '#d72323', '#d72323'] }
-    //     ];
-    //   })
+    this.five = e[0];
+    this.three = e[1];
+    this.two = e[2];
 
   }
 }
