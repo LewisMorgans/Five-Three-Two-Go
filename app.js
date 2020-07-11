f64af2176658114bcba43d5d4e37ddd5b5f12d32
Lewis Morgans <lmorgans90@gmail.com>
2020-07-11 16:04:31 +0100

    first

---
+++ app.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const mongo = require('mongodb');
+const passport = require('passport');
+const port =  process.env.PORT || 9090;
+require('dotenv').config();
+const bodyParser = require('body-parser');
+const authentication = require('./api/authentication-api');
+const data = require('./api/data-api');
+
+// Body-Parser middleware
+app.use(bodyParser.urlencoded({limit: '50mb'}));
+app.use(bodyParser.json({limit: '50mb'}));
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use('/api/authentication', authentication);
+app.use('/api/data/', data); 
+app.use(express.static(path.join(__dirname, '/client')))
+app.get('*/', (req, res) => { 
+    const indexFile = path.join(__dirname, '/client/index.html');
+    res.sendFile(indexFile);
+})
+
+app.listen(port, () => {
+    console.log(`Server running on ${port}`)
+})
