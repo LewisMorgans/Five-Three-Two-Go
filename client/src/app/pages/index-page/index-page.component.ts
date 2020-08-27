e5d13d5ce5944cb1776ba5eff72a4cea71d48bb9
Lewis Morgans <lmorgans90@gmail.com>
2020-08-27 14:12:56 +0100

    futher UI changes

--- client/src/app/pages/index-page/index-page.component.ts
+++ client/src/app/pages/index-page/index-page.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { AuthenticationService } from 'src/app/services/authentication/authentication.service';
 
 @Component({
   selector: 'app-index-page',
@@ -7,9 +10,54 @@ import { Component, OnInit } from '@angular/core';
 })
 export class IndexPageComponent implements OnInit {
 
-  constructor() { }
+  public loginForm: FormGroup;
+  public submitted = false;
+
+  constructor(
+    private readonly _fb: FormBuilder,
+    private readonly _router: Router,
+    private readonly _authenticationService: AuthenticationService) { }
 
   ngOnInit(): void {
+    this.intialiseFormState();
+    this._authenticationService.decodeToken()
   }
 
+  private intialiseFormState(): void {
+
+    this.loginForm = this._fb.group({
+      email: ['', Validators.required],
+      password: ['', Validators.required]
+    })
+  }
+
+  get f() {
+    return this.loginForm.controls;
+  }
+
+  public onSubmit(): void {
+    this.submitted = true;
+    if (this.validationCheck()) {
+      this._authenticationService.userLogin$(this.f.email.value, this.f.password.value)
+        .subscribe(resp => {
+          if (resp.status == 200) {
+            this._authenticationService.storeUserData(resp.token, resp.user)
+            this._authenticationService.loggedIn$()
+              .subscribe();
+            this._router.navigate(['finances'])
+          } else {
+            console.log('HIT ELSE BLOCK')
+          }
+        })
+
+    }
+  }
+
+  private validationCheck(): boolean {
+
+    if (this.loginForm.valid) {
+      return true;
+    } else return false;
+
+  }
 }
--- client/src/app/pages/index-page/index.module.ts
+++ client/src/app/pages/index-page/index.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { IndexPageComponent } from './index-page.component';
 import { IndexRoutingModule } from './index-routing.module';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [IndexPageComponent],
   imports: [
     CommonModule,
-    IndexRoutingModule
+    IndexRoutingModule,
+    ReactiveFormsModule
   ]
 })
 export class IndexModule { }
